01f46681afb562579a9a01801e6825bf9d356ff9
Natalia Castro <nat.alvz@gmail.com>
2018-05-14 21:23:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if is_upper?(phrase)
-    return ""
+    return "I LOVE YOU TOO PUMPKIN!"
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
